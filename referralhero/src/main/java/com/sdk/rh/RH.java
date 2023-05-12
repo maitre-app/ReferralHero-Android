784f40b8bb78e660dbce9cd09fc5458c4dfa4f0f
jayden <jayden@aistechnolabs.org>
2023-05-12 18:18:47 +0530

    Initial commit

---
+++ referralhero/src/main/java/com/sdk/rh/RH.java
@@ -0,0 +1,11 @@
+package com.sdk.rh;
+
+import android.content.Context;
+import android.util.Log;
+
+public class RH {
+
+    public void displayMsg(Context context){
+        Log.e("MSG","RH File");
+    }
+}
